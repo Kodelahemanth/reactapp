4fa224ce40ea76afb13da02c5eab882f3ccffff9
hemanthkodela <hemanth@voxglobaltech.com>
2023-09-27 18:46:36 +0530

    react app tabs

--- reactapp/src/Navbar.jsx
+++ reactapp/src/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HomePage from './components/HomePage'
 import Details from './components/Details'
 import Footer from './components/Footer'
 function Navbar() {
+    const [toggle, SetToggle] = useState(1)
+
+    function updateToggle(id) {
+        SetToggle(id)
+    }
   return (
     <>
     <nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
@@ -18,25 +23,36 @@ function Navbar() {
     </button>
     <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
       <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-        <li>
-          <button class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</button>
+        <li onClick={()=>updateToggle (1)}>
+          <button class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Home</button>
         </li>
-        <li>
+        <li onClick={()=>updateToggle (2)}>
           <button class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</button>
         </li>
-        <li>
+        <li onClick={()=>updateToggle (3)}>
           <button class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</button>
         </li>
-        <li> 
+        <li onClick={()=>updateToggle (4)}> 
           <button class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</button>
         </li>
       </ul>
     </div>
   </div>
 </nav>
+<div className={toggle === 1 ? "show-content" : "content"}>
 <HomePage/>
 <Details/>
 <Footer/>
+</div>
+<div className={toggle === 2 ? "show-content" : "content"}>
+    <h1 className='text-3xl text-gray-700 text-center my-16 font-semibold'>Services</h1>
+</div>
+<div className={toggle === 3 ? "show-content" : "content"}>
+    <h1 className='text-3xl text-gray-700 text-center my-16 font-semibold'>Pricing</h1>
+</div>
+<div className={toggle === 4 ? "show-content" : "content"}>
+    <h1 className='text-3xl text-gray-700 text-center my-16 font-semibold'>Contact</h1>
+</div>
 </>
 
   )
